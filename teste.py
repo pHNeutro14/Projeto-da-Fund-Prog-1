068f2794a53750ab962e7792d91eb4b81d38b278
Pedro Henrique <111392329+pHNeutro14@users.noreply.github.com>
2024-12-03 23:20:16 +0000

    Criação do menu

--- teste.py
+++ teste.py
@@ -1,1 +1,21 @@
-print("Olá")
+while True:
+    print("Selecione a opção: ")
+    print("1 - Calcular Notas")
+    print("2 - Verificar Notas")
+    print("3 - Exibir boletim")
+    print("0 - Encerrar Programa")
+
+    try:
+        escolha = int(input("> "))
+
+        if escolha == 1:
+            print("Opção 1 escolhida")
+        if escolha == 2:
+            print("Opção 2 escolhida")
+        if escolha == 3:
+            print("Opção 3 escolhida")
+        if escolha == 0:
+            break
+
+    except ValueError:
+        print("\nErro: Opção de entrada inválida!!\n")
