aacba6c3e43c2c2c07b6121ac620b24f544edf8d
Pedro Henrique <111392329+pHNeutro14@users.noreply.github.com>
2024-12-03 23:39:09 +0000

    Adição do else

--- teste.py
+++ teste.py
@@ -9,13 +9,16 @@ while True:
         escolha = int(input("> "))
 
         if escolha == 1:
-            print("Opção 1 escolhida")
-        if escolha == 2:
-            print("Opção 2 escolhida")
-        if escolha == 3:
-            print("Opção 3 escolhida")
-        if escolha == 0:
+            print("Opção 1 escolhida\n")
+        elif escolha == 2:
+            print("Opção 2 escolhida\n")
+        elif escolha == 3:
+            print("Opção 3 escolhida\n")
+        elif escolha == 0:
             break
+
+        else:
+            print("\nOpção Inválida, selecione outra opção!\n")
 
     except ValueError:
         print("\nErro: Opção de entrada inválida!!\n")
