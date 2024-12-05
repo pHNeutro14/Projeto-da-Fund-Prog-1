01072155ac05d2cc1cc4e85a083a5899d353cfaf
Pedro Henrique <111392329+pHNeutro14@users.noreply.github.com>
2024-12-05 12:35:54 +0000

    Atualização no menu e adição de opções

--- teste.py
+++ teste.py
@@ -1,8 +1,10 @@
 while True:
     print("Selecione a opção: ")
-    print("1 - Calcular Notas")
-    print("2 - Verificar Notas")
-    print("3 - Exibir boletim")
+    print("1 - Adicionar Alunos")
+    print("2 - Exibir Alunos")
+    print("3 - Calcular Notas")
+    print("4 - Verificar Notas")
+    print("5 - Exibir boletim")
     print("0 - Encerrar Programa \n")
 
     try:
@@ -14,6 +16,10 @@ while True:
             print("Opção 2 escolhida\n")
         elif escolha == 3:
             print("Opção 3 escolhida\n")
+        elif escolha == 4:
+            print("Opção 4 escolhida\n")
+        elif escolha == 5:
+            print("Opção 5 escolhida\n")
         elif escolha == 0:
             break
 
