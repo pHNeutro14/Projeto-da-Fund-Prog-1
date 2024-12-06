4390458404caea201da226dc2d209670ad876846
Pedro Henrique <111392329+pHNeutro14@users.noreply.github.com>
2024-12-06 12:39:13 +0000

    Criação das funções adicionar e exibir

--- teste.py
+++ teste.py
@@ -1,4 +1,21 @@
-while True:
+def adicionar_alunos(lista_alunos): 
+    while True: 
+        nome_aluno = input("Insira o nome do aluno - 0 para encerrar e voltar ao menu: \n> ")
+        if nome_aluno == "0":
+            break
+        else:
+            lista_alunos.append(nome_aluno)
+
+def exibir_alunos(lista_alunos):
+    if not lista_alunos:
+        print("Não existe alunos cadastrados!!\n")
+    else: 
+        for i in lista_alunos:
+            print(i)
+
+lista_alunos = [] #variável global para exibir e adicionar os alunos
+
+while True: #Menu criado para que o usuário possa interagir mais de uma vez até encerrar
     print("Selecione a opção: ")
     print("1 - Adicionar Alunos")
     print("2 - Exibir Alunos")
@@ -7,13 +24,15 @@ while True:
     print("5 - Exibir boletim")
     print("0 - Encerrar Programa \n")
 
-    try:
+    try: #caso o usuário use um valor diferente de um tipo inteiro, o try vai garantir que o código não encerre e pedir que o usuário digite novamente
         escolha = int(input("> "))
 
-        if escolha == 1:
+        if escolha == 1: #cada if vai levar para uma função diferente
             print("Opção 1 escolhida\n")
+            adicionar_alunos(lista_alunos)
         elif escolha == 2:
             print("Opção 2 escolhida\n")
+            exibir_alunos(lista_alunos)
         elif escolha == 3:
             print("Opção 3 escolhida\n")
         elif escolha == 4:
@@ -26,5 +45,5 @@ while True:
         else:
             print("\nOpção Inválida, selecione outra opção!\n")
 
-    except ValueError:
+    except ValueError: #caso o valor seja diferente do tipo inteiro vai mostrar um print informando a entrada inválida
         print("\nErro: Opção de entrada inválida!!\n")
