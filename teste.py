f2b0be861de579ca6df42848564a9498448ee7aa
Pedro Henrique <111392329+pHNeutro14@users.noreply.github.com>
2024-12-10 18:30:47 +0000

    Função excluir aluno quase que completa

--- teste.py
+++ teste.py
@@ -51,10 +51,16 @@ def exibir_boletim(lista_alunos):
             else:
                 print(f"Aluno: {nome} - Não possui notas cadastradas")
 
-def excluir_alunos():
-    nome_aluno = input("Insira o nome do aluno para excluir \n>").upper()
-    if nome_aluno not in lista_alunos:
-        print("Aluno não encontrado")
+def excluir_alunos(lista_alunos):
+    while True:
+        nome_aluno = input("Insira o nome do aluno para excluir - 0 para encerrar \n>").upper()
+        if nome_aluno == "0":
+            break
+        elif nome_aluno not in lista_alunos:
+            print("Aluno não encontrado")
+        else:
+            lista_alunos.pop(nome_aluno)
+            print("Aluno Removido")
 
 lista_alunos = {} # Dicionario para exibir e adicionar os alunos acompanhado de suas notas 
 
@@ -88,7 +94,7 @@ while True:  # Menu criado para que o usuário possa interagir mais de uma vez a
             exibir_boletim(lista_alunos)
         elif escolha == 6:
             print("Opção 6 escolhida \n")
-            excluir_aluno(lista_alunos)
+            excluir_alunos(lista_alunos)
         elif escolha == 0:
             break
 
